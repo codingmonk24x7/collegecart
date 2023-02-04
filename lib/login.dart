85501e7de8e2002fd64f4e8588b67403a802125f
Ramansh Agarwal <ramanshag001@gmail.com>
2023-02-04 15:35:26 +0530

    Modified login page

--- lib/login.dart
+++ lib/login.dart
@@ -15,6 +15,82 @@ class _MyloginState extends State<Mylogin> {
           fit: BoxFit.cover,
         ),
       ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: [
+            Container(
+              padding: EdgeInsets.only(top: 135, left: 60),
+              child: Text(
+                'Welcom\nback',
+                style: TextStyle(color: Colors.white, fontSize: 33.0),
+              ),
+            ),
+            SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.5,
+                    right: 35.0,
+                    left: 35.0),
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        fillColor: Colors.grey.shade400,
+                        filled: true,
+                        hintText: 'Email',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0)),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30.0,
+                    ),
+                    TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        fillColor: Colors.grey.shade400,
+                        filled: true,
+                        hintText: 'Password',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0)),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 50.0,
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        minimumSize: Size(180.0, 20.0),
+                        backgroundColor: Colors.blue,
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        'Login',
+                        style: TextStyle(color: Colors.white, fontSize: 25.0),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        minimumSize: Size(180.0, 20.0),
+                        backgroundColor: Colors.blue,
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        'Register',
+                        style: TextStyle(color: Colors.white, fontSize: 25.0),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
